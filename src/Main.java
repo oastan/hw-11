63d111b2683727652b27af6c89b63860155cc5e8
Oleg <oastan@ya.ru>
2024-07-23 12:53:56 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
+import javax.crypto.spec.PSource;
+import java.time.LocalDate;
+
 public class Main {
-    public static void main(String[] args) {
+    public static boolean isLeap(int year) {
+
+        return (year % 4 ==0 && year % 100 != 0) || (year % 400 ==0);
+    }
+    public static String installSystem(int os, int yearProd) {
+        int currentYear = LocalDate.now().getYear();
+        String msg = "Установите %s версию приложения для %s по ссылке";
+        String osName = os == 0 ? "iOS" : "Android";
+        String version = yearProd >= currentYear ? "" : "облегченную ";
+        return String.format(msg, version, osName);
+
 
     }
-}
+    public static int countDays(int deliveryDistance) {
+        int days = -1;
+        if (deliveryDistance <= 20) {
+            days ++;
+        } else if (deliveryDistance <= 60) {
+            days +=2;
+        } else if (deliveryDistance <= 100) {
+            days +=3;
+        }
+        return days;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Задание 1");
+        int year = 2012;
+        System.out.println(isLeap(year) && year % 438 ==0);
+
+        System.out.println("Задание 2");
+        //       installSystem(1, 2023);
+        System.out.println(installSystem(0, 2023));
+        System.out.println("Задание 3");
+        System.out.println(countDays(95));
+
+
+    }}
+
+
